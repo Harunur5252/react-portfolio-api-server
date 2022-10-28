d0eba6fc81b8a3f2a23bfaa0ff0901d440b387e4
Harunur5252 <hroshid695@gmil.com>
2022-10-29 02:00:38 +0600

    added slug code in category,tag,blog_post controller

--- src/api/tag/controllers/tag.js
+++ src/api/tag/controllers/tag.js
@@ -6,4 +6,14 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::tag.tag');
+module.exports = createCoreController('api::tag.tag',({strapi}) => ({
+    async findOne(ctx) {
+        const { id } = ctx.params;
+        const entity = await strapi.db.query('api::tag.tag').findOne({
+            where : { slug:id },
+        });
+        const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+    
+        return this.transformResponse(sanitizedEntity);
+      },
+}));
