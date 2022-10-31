f1959ffec0109f5a74ce71ecff00cade6c7d3787
Harunur5252 <hroshid695@gmil.com>
2022-10-31 22:08:37 +0600

    some change in plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -1,29 +1,30 @@
 module.exports = ({ env }) => ({
-    upload: {
-      config: {
-        provider: 'cloudinary',
-        providerOptions: {
-          cloud_name: env('CLOUDINARY_NAME'),
-          api_key: env('CLOUDINARY_KEY'),
-          api_secret: env('CLOUDINARY_SECRET'),
-        },
-        actionOptions: {
-          upload: {},
-          uploadStream: {},
-          delete: {},
-        },
+  email: {
+    config: {
+      provider: 'sendgrid',
+      providerOptions: {
+        apiKey: env('SENDGRID_API_KEY'),
+      },
+      settings: {
+        defaultFrom: 'hroshid695@gmail.com',
+        defaultReplyTo: 'hroshid695@gmail.com',
       },
     },
-    email: {
-      config: {
-        provider: 'sendgrid',
-        providerOptions: {
-          apiKey: env('SENDGRID_API_KEY'),
-        },
-        settings: {
-          defaultFrom: 'hroshid695@gmail.com',
-          defaultReplyTo: 'hroshid695@gmail.com',
-        },
+  },
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
       },
     },
-  });
+  },
+
+});
