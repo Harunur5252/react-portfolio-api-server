63bc22a3803093c560b37b7872025bd8a0dfff70
Harunur5252 <hroshid695@gmil.com>
2023-02-21 17:21:06 +0600

    added register link, update database section, pg package deleted

--- src/api/contact/content-types/contact/lifecycles.js
+++ src/api/contact/content-types/contact/lifecycles.js
@@ -1,21 +1,21 @@
 module.exports = {
-    async afterCreate(event) {    // Connected to "Save" button in admin panel
-        const { result } = event;
-
-        try{
-            await strapi.plugins['email'].services.email.send({
-              to: `${result?.email}`,
-              from: 'hroshid695@gmail.com', // e.g. single sender verification in SendGrid
-              subject: `${result?.subject}`, // Replace with a valid field ID
-              html: `
+  async afterCreate(event) {
+    // Connected to "Save" button in admin panel
+    const { result } = event;
+    try {
+      await strapi.plugins["email"].services.email.send({
+        to: `${result?.email}`,
+        from: "hroshid695@gmail.com", // e.g. single sender verification in SendGrid
+        subject: `${result?.subject}`, // Replace with a valid field ID
+        html: `
                <h1>Thank you ${result?.fullName} to message us</h1>
                <p>Your message is :  ${result?.description}</p>
-               <h4>If you are not registered , please register and see all blogs,manage dashboard etc.</h4> 
-              `, 
-            })
-        } catch(err) {
-            console.log(err);
-        }
+               <h4>If you are not registered , please register and see all blogs,manage dashboard etc. Here is the link : </h4> 
+               <h4>Here is register link : <a href='http://localhost:5173/register'>Click Here</a></h4> 
+              `,
+      });
+    } catch (err) {
+      console.log(err);
     }
-}
-
+  },
+};
